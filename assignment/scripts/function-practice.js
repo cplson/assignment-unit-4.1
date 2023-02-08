c7f6bbe0478122ea3c04f495a39635ee47ebf003
James Jacobsen <edjcjacobsen@gmail.com>
2023-02-08 16:10:33 -0600

    Completed 8

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -92,8 +92,7 @@ console.log(`Result of 1: ${find(13, myArray)}`);
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-  
-  
+  return (letter === string[0] ? true : false); 
 }
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
